866c4b973e9445599fc3ce11ce6191c0640620bf
Makarenko <makar44m@gmail.com>
2023-06-21 16:03:39 +0300

    Added new endpoint for color offsets

--- Backend/webserver.py
+++ Backend/webserver.py
@@ -24,7 +24,7 @@ def uploadFile():
     if file.filename == '':
         return 'No selected file', 400
 
-    file.save(f'../Frontend/static/images/{token}.png')
+    file.save(f'../Frontend/static/images/{token}.bmp')
 
     return redirect('/', 302)
 
@@ -32,14 +32,14 @@ def uploadFile():
 def invertImage():
     # startTime = time.time()
     token = request.form.get('token')
-    filePath = f"../Frontend/static/images/{token}.png"
+    filePath = f"../Frontend/static/images/{token}.bmp"
     img = Image.open(filePath).convert('RGB')
     imageParts, width, height = cropImage(img)
     threadManager(threadNumber=4, func=invertColors, args=(imageParts,))
     mergeImage(filePath, imageParts, width, height)
     # print(time.time()-startTime)
 
-    return f"/static/images/{token}.png"
+    return f"/static/images/{token}.bmp"
 
 tokens = {}    
     
@@ -47,14 +47,14 @@ tokens = {}
 def extendTime():
     token = request.form.get('token')
     currentTime = datetime.now()
-    tokens[token] = datetime.now() + timedelta(minutes=10)
+    tokens[token] = datetime.now() + timedelta(minutes=5)
 
     keysToRemove = []
 
     for key in tokens.keys():
         if tokens[key] <= currentTime:
-            if os.path.exists(f'../Frontend/static/images/{key}.png'):
-                os.remove(f'../Frontend/static/images/{key}.png')
+            if os.path.exists(f'../Frontend/static/images/{key}.bmp'):
+                os.remove(f'../Frontend/static/images/{key}.bmp')
                 keysToRemove.append(key)
             else:
                 keysToRemove.append(key) 
@@ -62,6 +62,36 @@ def extendTime():
     for key in keysToRemove:
         tokens.pop(key)    
 
-    return 'Time extended'            
+    return 'Time extended'  
+
+usersShifts = {}   
+
+@app.route('/colorshift', methods=['POST'])
+def colorshift():
+    token = request.form.get('token')   
+    if token not in usersShifts.keys():   
+        usersShifts[token] = [0, 0, 0]
+    value_R = request.form.get('value_R')   
+    if value_R:
+        usersShifts[token][0] = int(value_R)
+    value_G = request.form.get('value_G')   
+    if value_G:
+        usersShifts[token][1] = int(value_G)
+    value_B = request.form.get('value_B')
+    if value_B:
+        usersShifts[token][2] = int(value_B)
+    
+    # startTime = time.time()
+
+    filePath = f"../Frontend/static/images/{token}.bmp"
+    previewPath = f"../Frontend/static/preview_images/{token}.bmp"
+    img = Image.open(filePath).convert('RGB')
+    imageParts, width, height = cropImage(img)
+    threadManager(threadNumber=4, func=imageColorShift, args=(imageParts, usersShifts[token],))
+    mergeImage(previewPath, imageParts, width, height)
+
+    # print(time.time()-startTime)
+
+    return f"/static/images/{token}.bmp"
 
 app.run(host="0.0.0.0", port=8083, debug=True)
