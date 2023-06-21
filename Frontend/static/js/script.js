39997e28b44b3732e8374c3cff8aec8daec436cf
Makarenko <makar44m@gmail.com>
2023-06-21 16:01:19 +0300

    Added new js functions for color offsets

--- Frontend/static/js/script.js
+++ Frontend/static/js/script.js
@@ -85,3 +85,86 @@ window.setInterval(() => {
   extendLifeTime();
 }, 60000);
 
+let timeoutId;
+
+const input_R = document.getElementById('R-input');
+input_R.addEventListener('input', function() {
+  clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(function() {
+    const value_R = input_R.value;
+    const token = getToken();
+    const formData = new FormData();
+    formData.append("token", token);
+    formData.append("value_R", value_R);
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      if (xhttp.status === 200) {
+        console.log('Request successful');
+        var response = xhttp.responseText;
+
+        document.getElementById("preview-image").src = response + "?" + new Date().getTime();
+      } else {
+        console.error('Request failed. Error code: ' + xhttp.status);
+      }
+    }
+    xhttp.open("POST", "http://192.168.1.104:8083/colorshift", true);
+    xhttp.send(formData);
+  }, 500);
+});
+
+const input_G = document.getElementById('G-input');
+input_G.addEventListener('input', function() {
+  clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(function() {
+    const value_G = input_G.value;
+    const token = getToken();
+    const formData = new FormData();
+    formData.append("token", token);
+    formData.append("value_G", value_G);
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      if (xhttp.status === 200) {
+        console.log('Request successful');
+        var response = xhttp.responseText;
+
+        document.getElementById("preview-image").src = response + "?" + new Date().getTime();
+      } else {
+        console.error('Request failed. Error code: ' + xhttp.status);
+      }
+    }
+    xhttp.open("POST", "http://192.168.1.104:8083/colorshift", true);
+    xhttp.send(formData);
+  }, 500);
+});
+
+
+const input_B = document.getElementById('B-input');
+input_B.addEventListener('input', function() {
+  clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(function() {
+    const value_B = input_B.value;
+    const token = getToken();
+    const formData = new FormData();
+    formData.append("token", token);
+    formData.append("value_B", value_B);
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      if (xhttp.status === 200) {
+        console.log('Request successful');
+        var response = xhttp.responseText;
+
+        document.getElementById("preview-image").src = response + "?" + new Date().getTime();
+      } else {
+        console.error('Request failed. Error code: ' + xhttp.status);
+      }
+    }
+    xhttp.open("POST", "http://192.168.1.104:8083/colorshift", true);
+    xhttp.send(formData);
+  }, 500);
+});
