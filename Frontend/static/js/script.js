5cae15ab009bf273d0fc10a36e6d3ef19374b388
Makarenko <makar44m@gmail.com>
2023-06-26 22:02:42 +0300

    Added checking for existence of image before all ajax requests. Added function for convenient selecting of required extension with 'Tab'.

--- Frontend/static/js/script.js
+++ Frontend/static/js/script.js
@@ -16,6 +16,15 @@ function sendAPI(url, sendFunction, answerFunction) {
   xhttp.send(data);
 }
 
+function imageExists() {
+  const image = document.getElementById("preview-image").src
+  if (image !== "http://127.0.0.1:8081/") {
+    return true;
+  }else {
+    return false;
+  }
+}
+
 
 document.getElementById("image-upload").addEventListener("change", function(event) {
   var input = event.target;
@@ -43,7 +52,7 @@ document.getElementById("image-upload").addEventListener("change", function(even
         }
       };
 
-      xhttp.open("POST", "http://192.168.1.104:8083/upload", true);
+      xhttp.open("POST", "http://127.0.0.1:8083/upload", true);
       xhttp.send(formData);
     };
     reader.readAsDataURL(input.files[0]);
@@ -52,28 +61,31 @@ document.getElementById("image-upload").addEventListener("change", function(even
 
 
 function invertImage() {
-  sendAPI("http://192.168.1.104:8083/invertImage", 
-  function(){
-    const token = getToken();
-    const formData = new FormData();
-    formData.append("token", token);
-    return formData;
-  }, function(xhttp) {
-    if (xhttp.status === 200) {
-      console.log("File successfully uploaded to the server.");
-      var response = xhttp.responseText;
-
-      document.getElementById("preview-image").src = response + "?" + new Date().getTime();
-    } else {
-      console.log("Error uploading file to the server:", xhttp.status);
+  if (imageExists()) {
+    sendAPI("http://127.0.0.1:8083/invertImage", 
+    function(){
+      const token = getToken();
+      const formData = new FormData();
+      formData.append("token", token);
+      return formData;
+    }, function(xhttp) {
+      if (xhttp.status === 200) {
+        console.log("File successfully uploaded to the server.");
+        var response = xhttp.responseText;
+  
+        document.getElementById("preview-image").src = response + "?" + new Date().getTime();
+      } else {
+        console.log("Error uploading file to the server:", xhttp.status);
+      }
     }
+  )
   }
-)}
+  }
   
 
 
 function extendLifeTime() {
-  sendAPI("http://192.168.1.104:8083/extendLifeTime",
+  sendAPI("http://127.0.0.1:8083/extendLifeTime",
   function() {
     const token = getToken();
     const formData = new FormData();
@@ -103,11 +115,12 @@ greenInput.addEventListener('input', offsetRequest);
 blueInput.addEventListener('input', offsetRequest);
 
 function offsetRequest() {
-  
+  if (imageExists()) {
+      
   clearTimeout(timeoutId);
 
   timeoutId = setTimeout(function() {
-    sendAPI("http://192.168.1.104:8083/colorshift",
+    sendAPI("http://127.0.0.1:8083/colorshift",
     function() {
       const redValue = redInput.value;
       const greenValue = greenInput.value;
@@ -132,66 +145,74 @@ function offsetRequest() {
       }
     })
   }, 500);
+  }
 
 }
 
 function sumbitOffset() {
-  sendAPI("http://192.168.1.104:8083/submitOffset",
-  function() {
-    const token = getToken();
-    const formData = new FormData();
-    formData.append("token", token);
-    return formData;
-  }, function(xhttp) {
-    if (xhttp.status === 200) {
-      console.log("Request successful");
-      var response = xhttp.responseText;
+  if (imageExists()) {
+    sendAPI("http://127.0.0.1:8083/submitOffset",
+    function() {
+      const token = getToken();
+      const formData = new FormData();
+      formData.append("token", token);
+      return formData;
+    }, function(xhttp) {
+      if (xhttp.status === 200) {
+        console.log("Request successful");
+        var response = xhttp.responseText;
+  
+        document.getElementById("R-input").value = "";
+        document.getElementById("G-input").value = "";
+        document.getElementById("B-input").value = "";
+  
+        document.getElementById("preview-image").src = response + "?" + new Date().getTime();
+      }else {
+        console.error('Request failed. Error code: ' + xhttp.status);
+      }
+    });
+  }
 
-      document.getElementById("R-input").value = "";
-      document.getElementById("G-input").value = "";
-      document.getElementById("B-input").value = "";
-
-      document.getElementById("preview-image").src = response + "?" + new Date().getTime();
-    }else {
-      console.error('Request failed. Error code: ' + xhttp.status);
-    }
-  });
 }
 
 function undo() {
-  sendAPI("http://192.168.1.104:8083/undo",
-  function() {
-    const token = getToken();
-    const formData = new FormData();
-    formData.append("token", token);
-    return formData;
-  }, function(xhttp) {
-    if (xhttp.status === 200) {
-      console.log("Request successful");
-      var response = xhttp.responseText;
-      document.getElementById("preview-image").src = response + "?" + new Date().getTime();
-    }else {
-      console.error('Request failed. Error code: ' + xhttp.status);
-    }
-  });
+  if (imageExists()) {
+    sendAPI("http://127.0.0.1:8083/undo",
+    function() {
+      const token = getToken();
+      const formData = new FormData();
+      formData.append("token", token);
+      return formData;
+    }, function(xhttp) {
+      if (xhttp.status === 200) {
+        console.log("Request successful");
+        var response = xhttp.responseText;
+        document.getElementById("preview-image").src = response + "?" + new Date().getTime();
+      }else {
+        console.error('Request failed. Error code: ' + xhttp.status);
+      }
+    });
+  }
 }
 
 function redo() {
-  sendAPI("http://192.168.1.104:8083/redo", 
-  function() {
-    const token = getToken();
-    const formData = new FormData();
-    formData.append("token", token);
-    return formData;
-  }, function(xhttp) {
-    if (xhttp.status === 200) {
-      console.log("Request successful");
-      var response = xhttp.responseText;
-      document.getElementById("preview-image").src = response + "?" + new Date().getTime();
-    }else {
-      console.error('Request failed. Error code: ' + xhttp.status);
-    }
-  });
+  if (imageExists()) {
+    sendAPI("http://127.0.0.1:8083/redo", 
+    function() {
+      const token = getToken();
+      const formData = new FormData();
+      formData.append("token", token);
+      return formData;
+    }, function(xhttp) {
+      if (xhttp.status === 200) {
+        console.log("Request successful");
+        var response = xhttp.responseText;
+        document.getElementById("preview-image").src = response + "?" + new Date().getTime();
+      }else {
+        console.error('Request failed. Error code: ' + xhttp.status);
+      }
+    });
+  }
 }
 
 
@@ -206,34 +227,110 @@ document.addEventListener('keydown', function(event) {
     redo();
   }
 });
+var downloadLink = document.getElementById('downloadLink');
+downloadLink.setAttribute('disabled', 'disabled');
 
-
-
+var matches = [
+  "png","jpg","jpeg",
+  "bmp","tiff","tif",
+  "ico","webp","pbm",
+  "pgm","ppm","xbm",
+  "xv","ras","sr",
+  "tga","pcx","eps",
+  "ps","psd","tga"
+];
 const extensionInput = document.getElementById('extension-input');
 extensionInput.addEventListener('input', function(event) {
   var allExtensions = [
-    ".png",".jpg",".jpeg",
-    ".bmp",".tiff",".tif",
-    ".ico",".webp",".pbm",
-    ".pgm",".ppm",".xbm",
-    ".xv",".ras",".sr",
-    ".tga",".pcx",".eps",
-    ".ps",".psd",".tga"
+    "png","jpg","jpeg",
+    "bmp","tiff","tif",
+    "ico","webp","pbm",
+    "pgm","ppm","xbm",
+    "xv","ras","sr",
+    "tga","pcx","eps",
+    "ps","psd","tga"
   ];
-  const extensionValue = extensionInput.value;
-  var matches = [];
+  var extensionValue = extensionInput.value;
+  extensionValue = extensionValue.toLowerCase();
+  matches = [];
   document.getElementById("ext-input-status").textContent = "Not selected!";
 
   allExtensions.forEach(function(extension) {
-    extension = extension.toLowerCase();
     if (extension.includes(extensionValue)) {
       matches.push(extension)
       if (extensionValue === extension) {
-        document.getElementById("ext-input-status").textContent = "Saved!";
+        if (imageExists()) {
+          document.getElementById("ext-input-status").textContent = "Saved!";
+          sendAPI("http://127.0.0.1:8083/saveImage", 
+          function() {
+            const token = getToken();
+            const formData = new FormData();
+            formData.append("token", token);
+            formData.append("extension", extensionValue);
+            return formData;
+          }, function(xhttp) {
+            if (xhttp.status === 200) {
+              console.log("File successfully saved to the server.");
+              var response = xhttp.responseText;
+              const downloadHref = document.querySelector('.download');
+              downloadHref.href = response;
+              downloadLink.removeAttribute('disabled');
+            } else {
+              console.log("Error saving file to the server:", xhttp.status);
+            }
+          });
+        }else {
+          document.getElementById("ext-input-status").textContent = "Image not selected!";
+        }
+        
       }
     }
   });
   
   document.getElementById('available-extensions').textContent = matches.join(', ');
 
+});
+var index = 0;
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Tab') {
+    if (document.activeElement === extensionInput) {
+      event.preventDefault();
+      document.getElementById("extension-input").value = matches[index];
+      if (index+1 !== matches.length) {
+        index += 1;
+      }else {
+        index = 0;
+      }
+      if (imageExists()) {
+        document.getElementById("ext-input-status").textContent = "Saved!";
+        sendAPI("http://127.0.0.1:8083/saveImage", 
+        function() {
+          const token = getToken();
+          const formData = new FormData();
+          formData.append("token", token);
+          formData.append("extension", extensionInput.value);
+          return formData;
+        }, function(xhttp) {
+          if (xhttp.status === 200) {
+            console.log("File successfully saved to the server.");
+            var response = xhttp.responseText;
+            const downloadHref = document.querySelector('.download');
+            downloadHref.href = response;
+            downloadLink.removeAttribute('disabled');
+          } else {
+            console.log("Error saving file to the server:", xhttp.status);
+          }
+        });
+      }else {
+        document.getElementById("ext-input-status").textContent = "Image not selected!";
+      }
+        
+      
+    }
+  }
+});
+
+extensionInput.addEventListener('blur', function() {
+  index = 0;
 });
